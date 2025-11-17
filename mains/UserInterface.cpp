6de7c5a258ff86d001f235347287ebedb7346fbb
StaubMaster <dgerold@student.42heilbronn.de>
2025-11-17 22:04:17 +0100

    simple hardcoded Control Update

--- mains/UserInterface.cpp
+++ mains/UserInterface.cpp
@@ -12,6 +12,7 @@
 
 #include "DataShow.hpp"
 #include "Miscellaneous/ContainerDynamic.hpp"
+#include "Miscellaneous/EntryContainer/EntryContainerDynamic.hpp"
 
 #include "Graphics/Shader/Code.hpp"
 #include "Graphics/Shader/Base.hpp"
@@ -30,6 +31,7 @@
 
 
 Window * window;
+SizeRatio2D ViewPortSizeRatio;
 
 Shader::Base * UI_Shader;
 
@@ -52,12 +54,14 @@ struct UI_Inst_Data
 {
 	Point2D	Min;
 	Point2D	Max;
+	float	Layer;
 	Color Col;
 
 	UI_Inst_Data() { }
-	UI_Inst_Data(Point2D min, Point2D max, Color col) :
+	UI_Inst_Data(Point2D min, Point2D max, float layer, Color col) :
 		Min(min),
 		Max(max),
+		Layer(layer),
 		Col(col)
 	{ }
 };
@@ -85,6 +89,7 @@ class UI_Inst_Attribute : public Attribute::Base
 	private:
 		Attribute::Point2D	Min;
 		Attribute::Point2D	Max;
+		Attribute::FloatN	Layer;
 		Attribute::Color	Col;
 	public:
 		UI_Inst_Attribute(
@@ -92,10 +97,12 @@ class UI_Inst_Attribute : public Attribute::Base
 			unsigned int stride,
 			unsigned int indexMin,
 			unsigned int indexMax,
+			unsigned int indexLayer,
 			unsigned int indexCol
 		) :
 			Min(divisor, stride, indexMin),
 			Max(divisor, stride, indexMax),
+			Layer(divisor, stride, indexLayer),
 			Col(divisor, stride, indexCol)
 		{ }
 	public:
@@ -103,6 +110,7 @@ class UI_Inst_Attribute : public Attribute::Base
 		{
 			Min.Bind(offset);
 			Max.Bind(offset);
+			Layer.Bind(offset);
 			Col.Bind(offset);
 		}
 };
@@ -116,7 +124,7 @@ class UI_BufferArray : public BaseBufferArray
 	public:
 		UI_BufferArray() :
 			Main(1, (Attribute::Base * []) { new UI_Main_Attribute(0, sizeof(UI_Main_Data), 0) }),
-			Inst(1, (Attribute::Base * []) { new UI_Inst_Attribute(1, sizeof(UI_Inst_Data), 1, 2, 3) })
+			Inst(1, (Attribute::Base * []) { new UI_Inst_Attribute(1, sizeof(UI_Inst_Data), 1, 2, 3, 4) })
 		{ }
 
 	public:
@@ -128,9 +136,84 @@ class UI_BufferArray : public BaseBufferArray
 UI_BufferArray * UI_BufferArray_;
 
 ContainerDynamic<UI_Main_Data> UI_Main_Data_C;
-ContainerDynamic<UI_Inst_Data> UI_Inst_Data_C;
+EntryContainerDynamic<UI_Inst_Data> UI_Inst_Data_C;
 
+/*
+more in UI_Data
+{
+	Point2D NormalPos
+	Point2D PixelPos
+}
 
+Control
+{
+	Entry to UIInstData
+	NormalPos
+	PixelPos
+	Size ?
+
+	MinDist		Distance from Parent.Min to Control.Min
+	LenDist		Distance from Control.Min to Control.Max
+	MaxDist		Distance from Control.Max to Parent.Max
+}
+*/
+
+Point2D Normal0ToPixel(Point2D p)
+{
+	//	Normal [ 0 ; 1 ]
+	//	Pixel [ 0 ; n ]
+	p.X = p.X * ViewPortSizeRatio.W;
+	p.Y = p.Y * ViewPortSizeRatio.H;
+	return p;
+}
+Point2D PixelToNormal0(Point2D p)
+{
+	//	Pixel [ 0 ; n ]
+	//	Normal [ 0 ; 1 ]
+	p.X = p.X / ViewPortSizeRatio.W;
+	p.Y = p.Y / ViewPortSizeRatio.H;
+	return p;
+}
+Point2D Normal0ToNormal1(Point2D p)
+{
+	//	Normal0 [ 0 ; 1 ]
+	//	Normal1 [ -1 ; +1 ]
+	p.X = (p.X * 2) - 1;
+	p.Y = (p.Y * 2) - 1;
+	return p;
+}
+Point2D Normal1ToNormal0(Point2D p)
+{
+	//	Normal1 [ -1 ; +1 ]
+	//	Normal0 [ 0 ; 1 ]
+	p.X = (p.X + 1) / 2;
+	p.Y = (p.Y + 1) / 2;
+	return p;
+}
+
+#define ANCHOR_X_MASK 0b0011
+#define ANCHOR_X_NONE 0b0000
+#define ANCHOR_X_MIN  0b0001
+#define ANCHOR_X_MAX  0b0010
+#define ANCHOR_X_BOTH 0b0011
+
+class Control
+{
+	public:
+		EntryContainerDynamic<UI_Inst_Data>::Entry * Entry;
+		unsigned int Anchor;
+		Point2D	MinDist;
+		Point2D	LenDist;
+		Point2D	MaxDist;
+
+	public:
+		Control() { }
+		Control(EntryContainerDynamic<UI_Inst_Data>::Entry * entry) :
+			Entry(entry)
+		{ }
+};
+
+ContainerDynamic<Control> Controls;
 
 DirectoryContext ShaderDir("./media/Shaders");
 
@@ -156,9 +239,16 @@ void InitRun()
 	UI_Main_Data_C.Insert(UI_Main_Data(Point2D(-1, +1)));
 	UI_Main_Data_C.Insert(UI_Main_Data(Point2D(+1, +1)));
 
-	UI_Inst_Data_C.Insert(UI_Inst_Data(Point2D(-0.75, -0.75), Point2D(-0.25, -0.25), Color(1, 0, 0)));
-	UI_Inst_Data_C.Insert(UI_Inst_Data(Point2D(-0.25, -0.25), Point2D(+0.25, +0.25), Color(0, 1, 0)));
-	UI_Inst_Data_C.Insert(UI_Inst_Data(Point2D(+0.25, +0.25), Point2D(+0.75, +0.75), Color(0, 0, 1)));
+	unsigned int idx;
+
+	idx = Controls.Insert(Control(UI_Inst_Data_C.Alloc(1)));
+	(*Controls[idx].Entry)[0] = UI_Inst_Data(Point2D(), Point2D(), 0.2f, Color(1, 0, 0));
+	
+	idx = Controls.Insert(Control(UI_Inst_Data_C.Alloc(1)));
+	(*Controls[idx].Entry)[0] = UI_Inst_Data(Point2D(), Point2D(), 0.1f, Color(0, 1, 0));
+
+	idx = Controls.Insert(Control(UI_Inst_Data_C.Alloc(1)));
+	(*Controls[idx].Entry)[0] = UI_Inst_Data(Point2D(), Point2D(), 0.1f, Color(0, 0, 1));
 }
 void FreeRun()
 {
@@ -176,17 +266,48 @@ void Frame(double timeDelta)
 	(void)timeDelta;
 	UI_Shader -> Use();
 
+	//	Control 0
+	{
+		//	Fixed PixelSize
+		//	Stay in Center
+		Point2D	Size(480, 360);
+		Point2D NormalCenter(0, 0);
+		(*Controls[0].Entry)[0].Min = NormalCenter - PixelToNormal0(Size);
+		(*Controls[0].Entry)[0].Max = NormalCenter + PixelToNormal0(Size);
+	}
+
+	//	Control1
+	{
+		//	Fixed PixelSize
+		//	Keep Distance from BottomLeft of Control0
+		Point2D Dist(24, 24);
+		Point2D Size(240, 120);
+		(*Controls[1].Entry)[0].Min = (*Controls[0].Entry)[0].Min + PixelToNormal0(Dist);
+		(*Controls[1].Entry)[0].Max = (*Controls[0].Entry)[0].Min + PixelToNormal0(Dist) + PixelToNormal0(Size);
+	}
+
+	//	Control2
+	{
+		//	Fixed PixelSize
+		//	Keep Distance from TopRight of Control0
+		Point2D Dist(24, 24);
+		Point2D Size(240, 120);
+		(*Controls[2].Entry)[0].Min = (*Controls[0].Entry)[0].Max - PixelToNormal0(Dist) - PixelToNormal0(Size);
+		(*Controls[2].Entry)[0].Max = (*Controls[0].Entry)[0].Max - PixelToNormal0(Dist);
+	}
+
 	UI_BufferArray_ -> Use();
 	UI_BufferArray_ -> Main.BindData(GL_ARRAY_BUFFER, 0, sizeof(UI_Main_Data) * UI_Main_Data_C.Count(), UI_Main_Data_C.ToPointer(), GL_STREAM_DRAW);
 	UI_BufferArray_ -> Main.Count = UI_Main_Data_C.Count();
-	UI_BufferArray_ -> Inst.BindData(GL_ARRAY_BUFFER, 0, sizeof(UI_Inst_Data) * UI_Inst_Data_C.Count(), UI_Inst_Data_C.ToPointer(), GL_STREAM_DRAW);
-	UI_BufferArray_ -> Inst.Count = UI_Inst_Data_C.Count();
+	UI_BufferArray_ -> Inst.BindData(GL_ARRAY_BUFFER, 0, sizeof(UI_Inst_Data) * UI_Inst_Data_C.Length, UI_Inst_Data_C.Data, GL_STREAM_DRAW);
+	UI_BufferArray_ -> Inst.Count = UI_Inst_Data_C.Length;
 	UI_BufferArray_ -> Draw();
 }
 
 void Resize(int w, int h)
 {
-	Multi_ViewPortSizeRatio -> ChangeData(SizeRatio2D(w, h));
+	ViewPortSizeRatio = SizeRatio2D(w, h);
+	Multi_ViewPortSizeRatio -> ChangeData(ViewPortSizeRatio);
 }
 
 
--- media/Shaders/UI.vert
+++ media/Shaders/UI.vert
@@ -6,7 +6,8 @@ layout(location = 0) in vec2 Main_Pos;	//	square in range [ -1 , +1 ]
 
 layout(location = 1) in vec2 Inst_Min;
 layout(location = 2) in vec2 Inst_Max;
-layout(location = 3) in vec3 Inst_Col;
+layout(location = 3) in float Inst_Layer;
+layout(location = 4) in vec3 Inst_Col;
 
 out Vert_UI
 {
@@ -19,7 +20,7 @@ void main()
 {
 	vec2 Center = (Inst_Max + Inst_Min) / 2;
 	vec2 SizeHalf = (Inst_Max - Inst_Min) / 2;
-	gl_Position = vec4((Main_Pos * SizeHalf) + Center, 0, 1);
+	gl_Position = vec4((Main_Pos * SizeHalf) + Center, Inst_Layer, 1);
 
 	vs_out.Col = Inst_Col;
 }
