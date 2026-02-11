8541425342581157e144904753ff26d3e3e92319
Dustin Gerold <dgerold@3-D-10.42heilbronn.de>
2026-02-11 15:53:48 +0100

    basic working main for Physics

---
+++ Examples/Physics/main.cpp
@@ -0,0 +1,116 @@
+
+#include <iostream>
+#include <sstream>
+#include <exception>
+#include "OpenGL.hpp"
+#include "Debug.hpp"
+
+#include "DataInclude.hpp"
+#include "DataShow.hpp"
+
+#include "Window.hpp"
+#include "ValueType/View.hpp"
+#include "Function/Object.hpp"
+#include "UserParameter/KeyBoardInclude.hpp"
+#include "UserParameter/MouseInclude.hpp"
+
+#include "DirectoryInfo.hpp"
+#include "FileInfo.hpp"
+#include "Image.hpp"
+
+
+
+struct MainContext
+{
+DirectoryInfo ImageDir;
+DirectoryInfo ShaderDir;
+DirectoryInfo PolyHedraDir;
+DirectoryInfo TextDir;
+
+Window	window;
+View	view;
+
+MainContext() :
+	ImageDir("../../media/Images"),
+	ShaderDir("../../media/Shaders"),
+	PolyHedraDir("../../media/YMT"),
+	TextDir("../../media/Text"),
+	window(),
+	view()
+{ }
+~MainContext()
+{ }
+
+
+
+void Init()
+{
+
+}
+void Free()
+{
+}
+
+void Frame(double timeDelta)
+{
+	if (window.KeyBoardManager.Keys[GLFW_KEY_TAB].IsPress()) { window.MouseManager.CursorModeToggle(); }
+	if (window.MouseManager.CursorModeIsLocked())
+	{
+		Trans3D trans = window.MoveSpinFromKeysCursor();
+		if (window.KeyBoardManager.Keys[GLFW_KEY_LEFT_CONTROL].IsDown()) { trans.Pos *= 30; }
+		trans.Pos *= 2;
+		trans.Rot.X *= view.FOV.ToDegrees() * 0.005f;
+		trans.Rot.Y *= view.FOV.ToDegrees() * 0.005f;
+		trans.Rot.Z *= view.FOV.ToDegrees() * 0.005f;
+		view.TransformFlatX(trans, timeDelta);
+	}
+}
+void Resize(const WindowBufferSize2D & WindowSize)
+{
+	(void)WindowSize;
+}
+
+
+
+int Main()
+{
+	window.InitCallBack.Change<MainContext>(this, &MainContext::Init);
+	window.FreeCallBack.Change<MainContext>(this, &MainContext::Free);
+	window.FrameCallBack.Change<MainContext>(this, &MainContext::Frame);
+	window.ResizeCallBack.Change<MainContext>(this, &MainContext::Resize);
+
+	window.Create();
+	Debug::Log << "<<<< Run Window" << Debug::Done;
+	window.Run();
+	Debug::Log << ">>>> Run Window" << Debug::Done;
+	window.Delete();
+
+	return 0;
+}
+};
+
+
+
+int main(int argc, char * argv[])
+{
+	std::cout << "int main() ...\n";
+	int ret = -1;
+	Debug::NewFileInDir(DirectoryInfo("./logs/"));
+	if (argc > 0)	{ Debug::Log << argv[0] << Debug::Done; }
+	else			{ Debug::Log << "NoName" << Debug::Done; }
+	if (glfwInit() == 0) { std::cout << "GLFW Init Failed\n"; return -1; }
+	{
+		try
+		{
+			MainContext context;
+			ret = context.Main();
+		}
+		catch (std::exception & ex)
+		{ Debug::Log << "Error: " << ex.what() << Debug::Done; }
+		catch (...)
+		{ Debug::Log << "Error: " << "Unknown" << Debug::Done; }
+	}
+	glfwTerminate();
+	Debug::Log << "main() return " << ret << Debug::Done;
+	return ret;
+}
