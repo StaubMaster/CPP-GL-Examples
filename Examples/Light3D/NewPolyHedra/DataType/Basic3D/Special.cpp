20b278136f308581c87f4ae6ccbeb2ed31a506cb
Dustin Gerold <dgerold@3-E-13.42heilbronn.de>
2026-07-09 15:21:05 +0200

    specialized the whole Type_Object class. it works mostly like a normal .hpp and .cpp file now. havent seen this anywhere else so not sure if this is a hack or not?

--- Examples/Light3D/NewPolyHedra/DataType/Basic3D/Special.cpp
+++ Examples/Light3D/NewPolyHedra/DataType/Basic3D/Special.cpp
@@ -1,4 +1,5 @@
 #include "../../Type/Special.cpp"
+#include "../../Type/Special/Object.cpp"
 #include "ObjectData.hpp"
 
 
@@ -7,4 +8,6 @@ namespace NewPolyHedra
 {
 template Type_PalletObjectData<Basic3D::ObjectData> * Type_PalletObjectData<Basic3D::ObjectData>::Make(Pallet * pallet);
 template Type_PalletObjectData<Basic3D::ObjectData> * Type_PalletObjectData<Basic3D::ObjectData>::Make(PolyHedra * polyhedra);
+
+template struct Type_Object<Basic3D::ObjectData>;
 };
--- Examples/Light3D/NewPolyHedra/Type/Object.hpp
+++ Examples/Light3D/NewPolyHedra/Type/Object.hpp
@@ -1,105 +1,56 @@
 #ifndef  NEW_POLYHEDRA_TYPE_OBJECT_HPP
 # define NEW_POLYHEDRA_TYPE_OBJECT_HPP
 
-# include "PalletObjectData.hpp"
-
 class PolyHedra;
 
 namespace NewPolyHedra
 {
 struct Pallet;
 
-template<
-	typename TypeData
->
-struct Type_Object
+struct PalletObjectManager;
+
+template <typename TypeData> struct Type_PalletObjectData;
+
+template <typename TypeData> struct Type_Object
 {
 	private:
 	Type_PalletObjectData<TypeData> *		PalletObjectData;
 
 	public:
-	bool	Is() const
-	{
-		return (PalletObjectData != nullptr);
-	}
+	bool	Is() const;
 
 	public:
-	TypeData &	Data()
-	{
-		return (PalletObjectData -> Data);
-	}
-	const TypeData &	Data() const
-	{
-		return (PalletObjectData -> Data);
-	}
+	const	TypeData &	Data() const;
+			TypeData &	Data();
 
 	public:
-	void	ShowFull() { PalletObjectData -> ShowFull(); }
-	void	HideFull() { PalletObjectData -> HideFull(); }
-	bool	VisibleFull() const { return PalletObjectData -> VisibleFull(); }
+	bool	VisibleFull() const;
+	void	ShowFull();
+	void	HideFull();
 
 	public:
-	void	ShowWire() { PalletObjectData -> ShowWire(); }
-	void	HideWire() { PalletObjectData -> HideWire(); }
-	bool	VisibleWire() const { return PalletObjectData -> VisibleWire(); }
+	bool	VisibleWire() const;
+	void	ShowWire();
+	void	HideWire();
 
 	public:
-	PalletObjectManager * Manager()
-	{
-		return (PalletObjectData -> Manager);
-	}
-	const PalletObjectManager * Manager() const
-	{
-		return (PalletObjectData -> Manager);
-	}
+	const	PalletObjectManager *	Manager() const;
+			PalletObjectManager *	Manager();
 
 	public:
-	~Type_Object()
-	{
-		PalletObjectData -> Remove = true;
-		/*if (PalletObjectData != nullptr)
-		{
-			if (!PalletObjectData -> Remove)
-			{
-				PalletObjectData -> Remove = true;
-			}
-			else
-			{
-				delete PalletObjectData;
-			}
-		}*/
-	}
-	Type_Object()
-		: PalletObjectData(nullptr)
-	{ }
-	Type_Object(Pallet * pallet)
-		: PalletObjectData(Type_PalletObjectData<TypeData>::Make(pallet))
-	{ }
-	Type_Object(PolyHedra * polyhedra)
-		: PalletObjectData(Type_PalletObjectData<TypeData>::Make(polyhedra))
-	{ }
+	~Type_Object();
+	Type_Object();
+	Type_Object(Pallet * pallet);
+	Type_Object(PolyHedra * polyhedra);
 
 	public:
 	Type_Object(const Type_Object & other) = delete;
 	Type_Object & operator=(const Type_Object & other) = delete;
 
 	public:
-	void	Delete()
-	{
-		if (PalletObjectData == nullptr) { return; }
-		PalletObjectData -> Remove = true;
-		PalletObjectData = nullptr;
-	}
-	void	Create(Pallet * pallet)
-	{
-		if (PalletObjectData != nullptr) { return; }
-		PalletObjectData = Type_PalletObjectData<TypeData>::Make(pallet);
-	}
-	void	Create(PolyHedra * polyhedra)
-	{
-		if (PalletObjectData != nullptr) { return; }
-		PalletObjectData = Type_PalletObjectData<TypeData>::Make(polyhedra);
-	}
+	void	Delete();
+	void	Create(Pallet * pallet);
+	void	Create(PolyHedra * polyhedra);
 };
 };
 
